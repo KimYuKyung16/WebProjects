12b9c91b9c527faba86f48a4e2e15c0d6af4f442
kyk <kyg0954@naver.com>
2022-08-20 22:12:44 +0900

    React Plant Community Project
    Project Initialization

---
+++ React_plantcommunity/community/src/App.js
@@ -0,0 +1,16 @@
+import React, { useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Layout from "./layout/layout";
+
+function App() {
+
+  return (
+    <Layout>
+    
+    </Layout>
+  );
+}
+
+export default App;
